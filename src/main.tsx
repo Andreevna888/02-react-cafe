a52061b0d638de099aee7926c076f5f8e19137cf
tania <nominaomi888@gmail.com>
2026-09-06 14:18:43 +0300

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import 'modern-normalize';
+import 'modern-normalize/modern-normalize.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
