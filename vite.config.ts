26ff210fcaa5a5fe7009b30d5a210c86fdf72257
tania <nominaomi888@gmail.com>
2026-09-06 15:01:54 +0300

    fix hw

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/02-react-cafe/',
+  base: process.env.VERCEL ? '/' : '/02-react-cafe/',
 });
